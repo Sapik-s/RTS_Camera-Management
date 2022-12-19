c3bee2d3c19468ae209a0c2a98af3e4f8b272236
Sapik <Sapik_19@mail.ru>
2022-12-19 18:07:32 +0300

    Add Move to SelectionAdded the movement of the selected units.

---
+++ Source/Strategy/TheSystemOfIssuingCommandsToPawns/AIControllers/Main_AIController.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TheSystemOfIssuingCommandsToPawns/AIControllers/Main_AIController.h"
+
---
+++ Source/Strategy/TheSystemOfIssuingCommandsToPawns/AIControllers/Main_AIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Main_AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRATEGY_API AMain_AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
